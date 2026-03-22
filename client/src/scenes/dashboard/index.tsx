1bc750134ff72bb802ba6c3ec79a64580cfe07ca
Bambo0o0o <pramote.s@windowslive.com>
2026-03-22 19:06:58 +0700

    Complete steup dynamic layout on Dashboard page

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,5 +1,5 @@
 // Setup 01 : Simple dashboard page to show it work fine
-import {Box, useTheme} from '@mui/material'
+import {Box, useMediaQuery, useTheme} from '@mui/material'
 
 // type Props = {}
 // const Dashboard = (props: Props) => {
@@ -11,47 +11,7 @@ import {Box, useTheme} from '@mui/material'
 
 //Setup 02 : Grid layout as ---"static page"
 //Grid layout for Normal screen : Each letter is an elemeny on layouts which contain 10 elements
-const gridTemplate =`
-  "a b c"
-  "a b c"
-  "a b c"
-  "a b f"
-  "d e f"
-  "d e f"
-  "d h i"
-  "g h i"
-  "g h j"
-  "g h j"
-`
-const Dashboard = () => {
-  const {palette} = useTheme();
-  return (
-    // Adding responsive page with small-large page as ---"dynamic responsive page"
-    <Box width="100%" height="100%" display="grid"gap="1.5rem"
-      sx={{
-            gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-            gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-            gridTemplateAreas:gridTemplate,
-      }}
-    >
-    {/* Setup Dashboard to show layout which we create as a to j  */}
-      <Box bgcolor="#fff" gridArea="a"></Box>
-      <Box bgcolor="#fff" gridArea="b"></Box>
-      <Box bgcolor="#fff" gridArea="c"></Box>
-      <Box bgcolor="#fff" gridArea="d"></Box>
-      <Box bgcolor="#fff" gridArea="e"></Box>
-      <Box bgcolor="#fff" gridArea="f"></Box>
-      <Box bgcolor="#fff" gridArea="g"></Box>
-      <Box bgcolor="#fff" gridArea="h"></Box>
-      <Box bgcolor="#fff" gridArea="i"></Box>
-      <Box bgcolor="#fff" gridArea="j"></Box>
-    </Box>
-  )
-}
-
-
-// Setup 03 : Grid layout as ---"Dynamic page"
-// const gridTemplateLargeScreens = `
+// const gridTemplate =`
 //   "a b c"
 //   "a b c"
 //   "a b c"
@@ -62,76 +22,117 @@ const Dashboard = () => {
 //   "g h i"
 //   "g h j"
 //   "g h j"
-// `;
-// const gridTemplateSmallScreens = `
-//   "a"
-//   "a"
-//   "a"
-//   "a"
-//   "b"
-//   "b"
-//   "b"
-//   "b"
-//   "c"
-//   "c"
-//   "c"
-//   "d"
-//   "d"
-//   "d"
-//   "e"
-//   "e"
-//   "f"
-//   "f"
-//   "f"
-//   "g"
-//   "g"
-//   "g"
-//   "h"
-//   "h"
-//   "h"
-//   "h"
-//   "i"
-//   "i"
-//   "j"
-//   "j"
-// `;
-
-// const Dashboard = (props: Props) => {
+// `
+// const Dashboard = () => {
 //   const {palette} = useTheme();
 //   return (
-//     <Box
-//       width="100%"
-//       height="100%"
-//       display="grid"
-//       gap="1.5rem"
-//       sx={
-//         isAboveMediumScreens
-//           ? {
-//               gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-//               gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-//               gridTemplateAreas: gridTemplateLargeScreens,
-//             }
-//           : {
-//               gridAutoColumns: "1fr",
-//               gridAutoRows: "80px",
-//               gridTemplateAreas: gridTemplateSmallScreens,
-//             }
-//       }
+//     // Adding responsive page with small-large page as ---"dynamic responsive page"
+//     <Box width="100%" height="100%" display="grid"gap="1.5rem"
+//       sx={{
+//             gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+//             gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+//             gridTemplateAreas:gridTemplate,
+//       }}
 //     >
-//       {/* Setup Dashboard to show layout which we create as a to j  */}
-//         <Box bgcolor="#fff" gridArea="a"></Box>
-//         <Box bgcolor="#fff" gridArea="b"></Box>
-//         <Box bgcolor="#fff" gridArea="c"></Box>
-//         <Box bgcolor="#fff" gridArea="d"></Box>
-//         <Box bgcolor="#fff" gridArea="e"></Box>
-//         <Box bgcolor="#fff" gridArea="f"></Box>
-//         <Box bgcolor="#fff" gridArea="g"></Box>
-//         <Box bgcolor="#fff" gridArea="h"></Box>
-//         <Box bgcolor="#fff" gridArea="i"></Box>
-//         <Box bgcolor="#fff" gridArea="j"></Box>
+//     {/* Setup Dashboard to show layout which we create as a to j  */}
+//       <Box bgcolor="#fff" gridArea="a"></Box>
+//       <Box bgcolor="#fff" gridArea="b"></Box>
+//       <Box bgcolor="#fff" gridArea="c"></Box>
+//       <Box bgcolor="#fff" gridArea="d"></Box>
+//       <Box bgcolor="#fff" gridArea="e"></Box>
+//       <Box bgcolor="#fff" gridArea="f"></Box>
+//       <Box bgcolor="#fff" gridArea="g"></Box>
+//       <Box bgcolor="#fff" gridArea="h"></Box>
+//       <Box bgcolor="#fff" gridArea="i"></Box>
+//       <Box bgcolor="#fff" gridArea="j"></Box>
 //     </Box>
-//   );
-// };
+//   )
+// }
+
+
+//Setup 03 : Grid layout as ---"Dynamic page"
+const gridTemplateLargeScreens = `
+  "a b c"
+  "a b c"
+  "a b c"
+  "a b f"
+  "d e f"
+  "d e f"
+  "d h i"
+  "g h i"
+  "g h j"
+  "g h j"
+`;
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+`;
+
+const Dashboard = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
+  const {palette} = useTheme();
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
+    >
+      {/* Setup Dashboard to show layout which we create as a to j  */}
+        <Box bgcolor="#fff" gridArea="a"></Box>
+        <Box bgcolor="#fff" gridArea="b"></Box>
+        <Box bgcolor="#fff" gridArea="c"></Box>
+        <Box bgcolor="#fff" gridArea="d"></Box>
+        <Box bgcolor="#fff" gridArea="e"></Box>
+        <Box bgcolor="#fff" gridArea="f"></Box>
+        <Box bgcolor="#fff" gridArea="g"></Box>
+        <Box bgcolor="#fff" gridArea="h"></Box>
+        <Box bgcolor="#fff" gridArea="i"></Box>
+        <Box bgcolor="#fff" gridArea="j"></Box>
+    </Box>
+  );
+};
 
 // Setup 04 : Complete setup and Layout
 // import { Box, useMediaQuery } from "@mui/material";
